49f1e8490569a79f6fe66c4b22ef82469629366d
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-24 01:01:06 +0500

    Click Events

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,14 +5,10 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>Create</a>
+                <a href="/create">Create</a>
             </div>
         </nav>
     );
 }
-
+ 
 export default Navbar;
