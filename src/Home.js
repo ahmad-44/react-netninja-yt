e05f8faa86e9f69828d84684b12726fa86ce8a9e
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-26 02:03:01 +0500

    useEffect hook is used. it triggers on every render of its component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,9 +13,13 @@ const Home = () => {
         setBlogs(newBlogs)
     }
     
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+      })
     return(
         <div className="home">
-            <BlogList blogs={blogs} title={'All Blogs'} handleDelete={handleDelete}/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
