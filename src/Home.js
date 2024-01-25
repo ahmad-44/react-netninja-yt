5a977ec28640fd881fa058b1a385fa4d042afbf8
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-26 02:49:37 +0500

    Conditional loading message ....

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,25 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-        })
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false)
+            })
+        },1000)
       }, [])
 
     return(
         <div className="home">
-            {/* wait for the blogs to load after data fetch and then execute comonent based on condition */}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
