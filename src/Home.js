ac49d2fb845ed2880e635190605308d108a32bc7
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-25 23:26:20 +0500

    Used reactive variables. Used useState Hook to accomplish this

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25)
     const handleClick = (e) => {
-        console.log("Hello World!", e);
+        setName("Luigi");
+        setAge(30)
     }
     
-    const handleClickAgain = (name, e) => {
-        console.log("Hello " + name ,  e.target);
-    }
+    
     return(
         <div className="home">
-            <h2>Homepage</h2> 
+            <h2>Homepage</h2>
+            <p>{name} is {age} years old!</p> 
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e)=>handleClickAgain('Khan', e)}>Click Me Again</button>
         </div>
     );
 }
