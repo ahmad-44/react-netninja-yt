e19f3d0e50e6fe605bc35a7df406464f2eccfa1a
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-01-26 21:37:41 +0500

    Forcing routes to avoid server requests and use react to load the components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,11 @@
-// stateless function component (sfc)
+import {Link} from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">Create</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
